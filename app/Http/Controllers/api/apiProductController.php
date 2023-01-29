1794344031769d39b52a1eef4a32f01d2db06c1c
jose teixeira <josejonicaro@gmail.com>
2023-01-28 23:20:26 -0300

    base CRUD files

---
+++ app/Http/Controllers/api/apiProductController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
